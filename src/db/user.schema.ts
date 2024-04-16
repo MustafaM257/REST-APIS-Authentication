9b1689d68429b013f97ec36dbb2f2948e9d64812
Mustafa Muhammad <mustafa.m257@proton.me>
2024-04-15 20:32:36 -0400

    Add user schema, next work on auth utils

---
+++ src/db/user.schema.ts
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  authentication: {
+    password: {
+      type: String,
+      required: true,
+      select: false, // ! Avoid sending password in response
+    },
+    salt: {
+      type: String,
+      required: true,
+      select: false, // ! Avoid sending salt in response
+    },
+    sessionToekn: {
+      type: String,
+      required: false,
+      select: false, // ! Avoid sending session token in response
+    },
+  },
+});
+
+// * actions
+export const UserModel = mongoose.model("User", UserSchema);
+
+export const getUsers = () => UserModel.find();
+
+export const getUserByEmail = (email: string) => {
+  return UserModel.findOne({ email });
+};
+
+export const getUserBySessionToken = (sessionToken: string) => {
+  return UserModel.findOne({ "authentication.sessionToken": sessionToken });
+};
+
+export const getUserById = (id: string) => {
+  return UserModel.findById(id);
+};
+
+export const createUser = (user: Record<string, any>) => {
+  return new UserModel(user).save().then((user) => user.toObject());
+};
+
+export const deleteUserById = (id: string) => {
+  return UserModel.findByIdAndDelete({
+    _id: id,
+  });
+};
+
+export const updateUserById = (id: string, user: Record<string, any>) => {
+  return UserModel.findByIdAndUpdate(id, user);
+};
